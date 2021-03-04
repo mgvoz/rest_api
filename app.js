b14fd6fe630a3bafe08a10032e32eed7ef5e9789
Marissa <marissagvozdenovich@gmail.com>
2021-03-04 13:34:30 -0500

    set up routes and DB

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
 
-//ROUTES
-app.get('/', (req, res) => {
-	res.send('we are on home');
-});
+app.use(bodyParser.json());
+
+//IMPORT ROUTES, USE MIDDLEWARE
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
+
+//CONNECT DB
+mongoose.connect(
+	process.env.DB_CONNECTION,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => console.log('connected to db'),
+);
 
 app.listen(3000);
